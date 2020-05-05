d2485b74d63cd1e3221694378f12072d11c53a1e
Loïc Dumont-Roty <loic.dumont-roty@epitech.eu>
2020-05-05 14:19:56 +0200

    Lowlow: Préparation repo cooker

---
+++ src/cooker/cooker.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2020
+** Delivery
+** File description:
+** cooker
+*/
+
+#ifndef COOKER_HPP_
+#define COOKER_HPP_
+#include "../my.hpp"
+#include "../pizza/Pizza.hpp"
+
+class Cooker
+{
+private:
+    /* data */
+public:
+    Cooker(/* args */);
+    ~Cooker();
+};
+
+Cooker::Cooker(/* args */)
+{
+}
+
+Cooker::~Cooker()
+{
+}
+
+#endif /* !COOKER_HPP_ */
